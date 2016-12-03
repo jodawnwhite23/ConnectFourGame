23321fb3594b23e87a21ea46dcaf0fceb363fc81
JoDawn White <jwhite13@asu.edu>
2016-12-03 13:12:34 -0700

    Play again enhancement added to Game.java

--- src/connect/four/Game.java
+++ src/connect/four/Game.java
@@ -2,8 +2,13 @@
 package connect.four;
 
 import connect.four.player.Player;
+import connect.four.player.ConsolePlayer;
 import connect.four.board.ReadableBoard;
 import connect.four.board.ReadWritableBoard;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -49,29 +54,37 @@ public class Game implements ScoreChart {
     void performPlay(final int player) {
         m_currentPlayer = player;
         ReadWritableBoard controlledBoard = new ReadWritableBoard() {
-            boolean played;
+            //boolean played;
             @Override public Player whoPlayed(int x, int y) {
                 return m_board.whoPlayed(x, y);
             }
-            @Override public void play(int x, Player p) {
-                if (played) {
-                    throw new Error(p+" Played more than once in a turn.");
-                }
-                played = true;
+            @Override public void play(int x, Player p) {         	
                 m_board.play(x, p);
-		Player win = detectWinner(m_board, m_inRow);
+                Player win = detectWinner(m_board, m_inRow);
                 if (win != null) {
                     m_scores[player] += 1;
-                    for (ScoreChart.Listener l : m_listeners) {
-                        l.gameOver(win, Game.this, m_board);
-                    }
+                    //Begin code modified by Group 22
+                    clearScreen();
+                    System.out.println(win.getName() + "Won!!!");
+                    printPlayerPieceAssociations();
+                    System.out.println("Final board state: ");
+                    printBoard(m_board);
+                    printScores();
+                  //End code modified by Group 6
                     m_board.clear();
+                    playAgainPrompt();//code modified by Group 6
                     performPlay(player);
 		} else if (m_board.getMoveCount() == m_board.getWidth()*m_board.getHeight() ) {
-                    for (ScoreChart.Listener l : m_listeners) {
-                        l.gameOver(null, Game.this, m_board);
-                    }
+					//Begin code modified by Group 6:
+		        	clearScreen();
+		        	System.out.println("Draw! Nobody wins.");
+		        	printPlayerPieceAssociations();
+		        	System.out.println("Final board state:");
+		            printBoard(m_board);
+		            printScores();
+		            //End of modification
                     m_board.clear();
+                    playAgainPrompt(); //code modified by Group 22
                     performPlay((player+1) % m_players.length);
                 } else {
                     performPlay((player+1) % m_players.length);
@@ -93,6 +106,14 @@ public class Game implements ScoreChart {
 		return m_board.getMoveCount();
 	    }
         };
+        
+        //Begin code modified by Group 6:
+        clearScreen();
+        printPlayerPieceAssociations();
+        System.out.println(m_players[player].getName() + "'s turn!");
+        System.out.println();
+        printBoard(m_board);
+        //End code modified by Group 6
         m_players[player].performPlay(controlledBoard);
     }
     
@@ -179,4 +200,89 @@ public class Game implements ScoreChart {
 	}
         return null;
     }
+    
+    /**
+     * Prompt the player(s) and ask them if they
+     * want to play again. Modification added 11-29-2016
+     */
+    private void playAgainPrompt(){
+    	System.out.println();
+        System.out.println("Would you like to play again?");
+        System.out.println("(Enter 0 for quit and 1 for play again)");
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        int x = -1;
+        while (x < 0 || x > 1) {
+            try {
+                System.out.print("Enter your selection: ");
+                x = Integer.parseInt(stdin.readLine());
+            } catch (IOException e) {
+                // loop again.
+            } catch (NumberFormatException e) {
+                // loop again.
+            }
+        }
+        if(x == 0)
+            System.exit(0);
+        else
+        	System.out.println("Playing again!");
+    }
+    
+    /**
+     * Print the board. Modification 11-29-2016
+     * Taken from class ConsolePlayer (where it is called dumpBoard)
+     * @param board
+     */
+    private void printBoard(ReadableBoard board) {
+        int width = board.getWidth();
+        int height = board.getHeight();
+
+        for (int i = height-1; i != -1; --i) {
+            for (int j = 0; j != width; ++j) {
+                Player played = board.whoPlayed(j, i);
+                System.out.print(
+                    played == m_players[0] ? "@ " :
+                    played == null ? "O " : "X "
+                );
+            }
+            System.out.println();
+        }
+        for (int i = 0; i != width-1; ++i) {
+            System.out.print("--");
+        }
+        System.out.print("-");
+        System.out.println();
+        for (int i = 0; i != width; ++i) {
+            System.out.print(i+1 + " ");
+        }
+        System.out.println();
+    }
+    
+    /**
+     * Helper method to print the scores. Modification added
+     * 11-29-2016
+     */
+    private void printScores(){
+    	System.out.println("Scores:");
+        System.out.println(m_players[0].getName() + ": " + getScore(m_players[0]));
+        System.out.println(m_players[1].getName() + ": " + getScore(m_players[1]));
+    }
+    
+    /**
+     * Helper method to clear the screen by 
+     * printing many newlines. Modification 11-29-2016
+     */
+    private void clearScreen(){
+    	for(int i = 0; i < 50; i++)
+    		System.out.println();
+    }
+    
+    /**
+     * Helper method to print which pieces belong to which players.
+     */
+    private void printPlayerPieceAssociations() {
+        System.out.println(m_players[0].getName() + ": @");
+    	System.out.println(m_players[1].getName() + ": X");
+    	System.out.println();
+    }
+
 }
